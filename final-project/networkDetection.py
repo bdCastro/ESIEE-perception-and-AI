b3bd2cd63ee96787f44442dced35edfbe52f1684
VSundermann <65927923+VSundermann@users.noreply.github.com>
2024-02-07 17:07:19 +0100

    Updated network section

--- final-project/networkDetection.py
+++ final-project/networkDetection.py
@@ -12,7 +12,6 @@ from skimage.feature import peak_local_max
 from skimage.segmentation import watershed
 from PIL import Image
 import glob
-#import imutils
 
 from ultralytics import YOLO
 
@@ -20,20 +19,13 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model.load('bdCastro/ESIEE-perception-and-AI/final-project/data/best.pt')
 
-for filename in glob.glob('./data/WALK_Y-F0-B1_P287619_20200619084759_250/*.png'):
-    frame=cv2.imread(filename)
-    # Normalize the image
-    img_normalized = cv2.normalize(frame, None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    cv2.imwrite(filename, img_normalized)
+#for filename in glob.glob('./data/WALK_Y-F0-B1_P287619_20200619084759_250/*.png'):
+#    frame=cv2.imread(filename)
+#    # Normalize the image
+#    img_normalized = cv2.normalize(frame, None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+#    cv2.imwrite(filename, img_normalized)
 
 for filename in glob.glob('./data/WALK_Y-F0-B1_P287619_20200619084759_250/*.png'):
     res = model.predict(filename)
     res_plotted = res[ 0 ].plot() # get results
-
-    #Count people in image
-    count = 0
-    for i in range(len(res_plotted)):
-        if res_plotted[ i ].label == 'person':
-            count += 1
-
     cv2.imshow('People', res_plotted)
